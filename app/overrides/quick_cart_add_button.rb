7355e27775b0431f35f54d557ffd4fca9a91d97e
RostDetal <info@rostdetal.ru>
2014-07-02 23:12:06 +0400

    Правки по quick cart

--- app/overrides/quick_cart_add_button.rb
+++ app/overrides/quick_cart_add_button.rb
@@ -1,11 +1,6 @@
-Deface::Override.new(:virtual_path => "spree/shared/content/_products_all",
+Deface::Override.new(:virtual_path => "spree/shared/content/_products_list",
                      :name => "quick_cart_add_button",
                      :insert_after => "#product-price-block span.price-cost",
                      :partial => "spree/shared/quick_cart_add_button",
                      :disabled => false)
 
-Deface::Override.new(:virtual_path => "spree/shared/content/_new_and_recommended",
-                     :name => "quick_cart_add_button",
-                     :insert_after => "#product-price-block span.price-cost",
-                     :partial => "spree/shared/quick_cart_add_button",
-                     :disabled => false)
