e99f637ee1f9d94f5cf77f01a8952e91a0c16536
RostDetal <info@rostdetal.ru>
2015-03-19 14:08:00 +0300

    Fix route duplicating

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
   namespace :api do
-    get '/current_order', to: 'orders#current', :as "quick_current_order"
+    get '/current_order', to: 'orders#current', as: "quick_current_order"
   end
 end
