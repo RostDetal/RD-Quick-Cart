cf971b3096095f2ee8764def6cb1252e7710a1c1
RostDetal <info@rostdetal.ru>
2015-03-19 14:33:23 +0300

    order

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
   namespace :api do
-    get '/current_order', to: 'orders#current'
+    get '/current_order', to: 'orders#current', as: "quick_current_order"
   end
 end
